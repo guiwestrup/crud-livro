15d807c287b406a5aa7a69442e52b85e2b455f1e
Guilherme Westrup <westrupguilherme@gmail.com>
2018-10-23 20:45:50 -0300

    first commit

---
+++ src/banco/ui/InterfaceLivroTexto.java
@@ -0,0 +1,103 @@
+package banco.ui;
+
+import java.util.List;
+
+import banco.dao.AutorDao;
+import banco.dao.LivroDao;
+import banco.modelo.Autor;
+import banco.modelo.Livro;
+
+public class InterfaceLivroTexto extends InterfaceModeloTexto {
+	
+	private LivroDao dao;
+	private AutorDao autorDao;
+	
+	public InterfaceLivroTexto() {
+		super();
+		dao = new LivroDao();
+	}
+	
+	private Livro obtemDadosLivro(Livro livro) {
+		
+		System.out.print("Insira o título do livro: ");
+		String titulo = entrada.nextLine();
+		
+		System.out.println("Insira o ano de publicação (somente números): ");
+		int anoPublicacao = entrada.nextInt();
+		entrada.nextLine();
+		
+		System.out.println("Insira o nome da editora: ");
+		String editora = entrada.nextLine();
+
+		System.out.println("Insira o id do autor: ");
+		int autor_id = entrada.nextInt();
+
+		Autor autor = autorDao.getByKey(autor_id);
+
+		Livro novoLivro = new Livro(0, titulo, anoPublicacao, editora, autor);
+		
+		return novoLivro;
+	}
+	
+	@Override
+	public void adicionar() {
+		System.out.println("Adicionar livro");
+		System.out.println();
+		
+		Livro novoLivro = obtemDadosLivro(null);
+		dao.insert(novoLivro);
+		
+	}
+
+	@Override
+	public void listarTodos() {
+		List<Livro> livros = dao.getAll();
+		
+		System.out.println("Lista de livros");
+		System.out.println();
+		
+		System.out.println("id\tTítulo\tAno publicação\tEditora\tAutor");
+
+		for (Livro livro : livros) {
+			imprimeItem(livro);
+		}
+		
+	}
+
+	@Override
+	public void editar() {
+		listarTodos();
+		
+		System.out.println("Editar livro");
+		System.out.println();
+		
+		System.out.print("Entre o id do livro: ");
+		int id = entrada.nextInt();
+		entrada.nextLine();
+		
+		Livro livroAModifcar = dao.getByKey(id);
+		
+		Livro novoLivro = obtemDadosLivro(livroAModifcar);
+		
+		novoLivro.setId(id);
+		
+		dao.update(novoLivro);
+		
+	}
+
+	@Override
+	public void excluir() {
+		listarTodos();
+		
+		System.out.println("Excluir livro");
+		System.out.println();
+		
+		System.out.print("Entre o id do livro: ");
+		int id = entrada.nextInt();
+		entrada.nextLine();
+		
+		dao.delete(id);
+		
+	}
+
+}
---
+++ src/banco/ui/InterfaceTexto.java
@@ -0,0 +1,107 @@
+package banco.ui;
+
+import java.util.Scanner;
+
+public class InterfaceTexto {
+	private Scanner entrada;
+	private Estado estadoAtual;
+	
+	private static final int OP_LIVRO = 1;
+	private static final int OP_AUTOR = 2;
+	private static final int OP_SAIR = 0;
+	private static final int OP_ADICIONAR = 1;
+	private static final int OP_EDITAR = 2;
+	private static final int OP_EXCLUIR = 3;
+	private static final int OP_LISTAR = 4;
+	
+	private enum Estado {PRINCIPAL, LIVRO, AUTOR};
+	
+	public InterfaceTexto() {
+		entrada = new Scanner(System.in);
+		estadoAtual = Estado.PRINCIPAL;
+	}
+	
+	private void imprimeMenu() {
+		System.out.println("CRUD LIVROS");
+		System.out.println();
+		
+		switch (estadoAtual) {
+		case LIVRO:
+			imprimeMenuLivro();
+			break;
+		case AUTOR:
+			imprimeMenuAutor();
+			break;
+		default:
+			imprimeMenuPrincipal();
+		}
+		System.out.println();
+		System.out.println("0 - Sair");
+		
+		System.out.println();
+		System.out.print("Escolha uma opção: ");	
+	}
+	
+	private int leOpcao() {
+		int opcao = entrada.nextInt();
+		entrada.nextLine();
+		return opcao;
+	}
+	
+	private void imprimeMenuPrincipal() {
+		System.out.println("1 - Administração de Livros");
+		System.out.println("2 - Administração de Autores");
+	}
+	
+	private void imprimeMenuLivro() {
+		System.out.println("1 - Adicionar livro");
+		System.out.println("2 - Editar livro");
+		System.out.println("3 - Excluir livro");
+		System.out.println("4 - Listar livros");
+	}
+	
+	private void imprimeMenuAutor() {
+		System.out.println("1 - Adicionar autor");
+		System.out.println("2 - Editar autor");
+		System.out.println("3 - Excluir autor");
+		System.out.println("4 - Listar autores");
+	}
+		
+	public void executa() {
+		InterfaceModeloTexto subMenu;
+		
+		imprimeMenu();
+		int opcao = leOpcao();
+		
+		while (opcao != OP_SAIR) {
+			if (estadoAtual == Estado.PRINCIPAL) {
+				estadoAtual = opcao == OP_LIVRO ? Estado.LIVRO : Estado.AUTOR;
+			} else {
+				subMenu = estadoAtual == Estado.LIVRO ?
+						new InterfaceLivroTexto() : new InterfaceAutorTexto();
+				
+				switch (opcao) {
+				case OP_ADICIONAR:
+					subMenu.adicionar();
+					break;
+				case OP_EDITAR:
+					subMenu.editar();
+					break;
+				case OP_EXCLUIR:
+					subMenu.excluir();
+					break;
+				case OP_LISTAR:
+					subMenu.listarTodos();
+					break;
+				default:
+					System.out.println("Opção Inválida. Tente novamente!");
+				}
+			}
+			
+			imprimeMenu();
+			opcao = leOpcao();
+		}
+		
+	}
+	
+}
